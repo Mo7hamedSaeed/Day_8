094cb106f0abad14190bfbf25a5a472e79f6f40f
mo7hamedsaeed <mo7hamedsaeed@gmail.com>
2023-08-30 05:17:55 +0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MenComponent } from './men/men.component';
+import { WomenComponent } from './women/women.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { MenProductsComponent } from './men-products/men-products.component';
+import { WomenProductsComponent } from './women-products/women-products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'men',component:MenComponent},
+  {path:'menproducts/:id',component:MenProductsComponent},
+  {path:'women',component:WomenComponent},
+  {path:'womenproducts/:id',component:WomenProductsComponent},
+  {path:'**',component:NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { MenComponent } from './men/men.component';
+import { WomenComponent } from './women/women.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { MenProductsComponent } from './men-products/men-products.component';
+import { WomenProductsComponent } from './women-products/women-products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    MenComponent,
+    WomenComponent,
+    HomeComponent,
+    NotFoundComponent,
+    MenProductsComponent,
+    WomenProductsComponent,
   ],
   imports: [
     BrowserModule,
